0191187c80172b898f37819ddd45474de163f668
yashi8596 <deletio.422shinta@gmail.com>
2022-08-18 10:15:40 +0000

    [Finish] App1-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -22,14 +22,20 @@ class ListsController < ApplicationController
   end
 
   def update
-    @list = List.find(params[:id])
-    @list.update(list_params)
-    redirect_to list_path(@list.id)
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+  
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
   end
 
   private
 
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
